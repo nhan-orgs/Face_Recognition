a18ec60b199546a1243ff2f27e9f2fb7f6d6ce07
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-08-31 13:31:27 +0700

    connect load and save face

--- save_face/script.js
+++ save_face/script.js
@@ -1,55 +1,52 @@
-const video = document.getElementById("video");
-const btnTakeImg = document.getElementById("button");
+const video = document.getElementById('video')
+const btnTakeImg = document.getElementById('button')
 
 navigator.mediaDevices
-  .getUserMedia({ video: true })
-  .then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  })
-  .catch((error) => {
-    console.log("An error has occurred: ", error);
-  });
+    .getUserMedia({ video: true })
+    .then((stream) => {
+        video.srcObject = stream
+        video.play()
+    })
+    .catch((error) => {
+        console.log('An error has occurred: ', error)
+    })
 
 const loadFaceAPI = async () => {
-  await Promise.all([
-    faceapi.loadSsdMobilenetv1Model("./models"),
-    faceapi.loadFaceRecognitionModel("./models"),
-    faceapi.loadFaceLandmarkModel("./models"),
-  ]);
+    await Promise.all([
+        faceapi.loadSsdMobilenetv1Model('./models'),
+        faceapi.loadFaceRecognitionModel('./models'),
+        faceapi.loadFaceLandmarkModel('./models'),
+    ])
 
-  Toastify({
-    text: "Models loaded successfully",
-  }).showToast();
-};
+    Toastify({
+        text: 'Models loaded successfully',
+    }).showToast()
+}
 
-btnTakeImg.addEventListener("click", loadFaceAPI);
+btnTakeImg.addEventListener('click', takeImage)
 
-let faceDescriptors = ["Viet"];
+let faceDescriptors = ['Viet']
 
 async function takeImage() {
-  await loadFaceAPI();
+    await loadFaceAPI()
 
-  let detects = await faceapi
-    .detectSingleFace(video)
-    .withFaceLandmarks()
-    .withFaceDescriptor(); // Tested
+    let detects = await faceapi.detectSingleFace(video).withFaceLandmarks().withFaceDescriptor() // Tested
 
-  console.log(detects.descriptor);
-  faceDescriptors.push(detects.descriptor);
-  console.log(faceDescriptors);
+    console.log(detects.descriptor)
+    faceDescriptors.push(detects.descriptor)
+    console.log(faceDescriptors)
 
-  if (faceDescriptors.length > 4) {
-    downloadDescriptorsAsJSON();
-  }
-  // Save the descriptor
+    if (faceDescriptors.length > 4) {
+        downloadDescriptorsAsJSON()
+    }
+    // Save the descriptor
 }
 
 async function downloadDescriptorsAsJSON() {
-  const descriptorsArray = Array.from(faceDescriptors);
-  const jsonData = JSON.stringify(descriptorsArray);
-  const res = await axios.post(`http://localhost:8000/save`, {
-    data: jsonData,
-  });
-  console.log(res);
+    const descriptorsArray = Array.from(faceDescriptors)
+    const jsonData = JSON.stringify(descriptorsArray)
+    const res = await axios.post(`http://localhost:8000/save`, {
+        data: jsonData,
+    })
+    console.log(res)
 }
